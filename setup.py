992b686364c6827602085b3964375da443a11b23
SanmiLee <sanmi4ariyo@gmail.com>
2020-04-09 19:59:57 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaus_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_dist'],
+      author='Ariyo Sanmi',
+      author_email = 'sanmi4ariyo@gmail.com',
+      zip_safe=False)
